1303c1c6f1832c142c5d18b75673d7f00724a162
ADITHYA SAI <60120972+neeliadithyasai@users.noreply.github.com>
2020-04-13 15:35:04 -0400

    [AN] Added variables in bill class.

--- app/src/main/java/com/example/c0771910_w2020_mad3125_fp/classes/Bill.java
+++ app/src/main/java/com/example/c0771910_w2020_mad3125_fp/classes/Bill.java
@@ -1,4 +1,12 @@
 package com.example.c0771910_w2020_mad3125_fp.classes;
 
+
 public class Bill {
+
+
+    private String billId;
+    private String billDate;
+    private String billType;
+    private Double billAmount;
+
 }
