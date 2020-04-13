2e3184273987ddd27dee9d0e8cfdf339871fe160
ADITHYA SAI <60120972+neeliadithyasai@users.noreply.github.com>
2020-04-13 15:39:55 -0400

    [AN] implemented Idisplay in customer and bills.

--- app/src/main/java/com/example/c0771910_w2020_mad3125_fp/classes/Bill.java
+++ app/src/main/java/com/example/c0771910_w2020_mad3125_fp/classes/Bill.java
@@ -1,7 +1,7 @@
 package com.example.c0771910_w2020_mad3125_fp.classes;
 
 
-public class Bill {
+public class Bill implements Idisplay {
 
 
     private String billId;
@@ -9,4 +9,15 @@ public class Bill {
     private String billType;
     private Double billAmount;
 
+    public Bill(String billId, String billDate, String billType, Double billAmount) {
+        this.billId = billId;
+        this.billDate = billDate;
+        this.billType = billType;
+        this.billAmount = billAmount;
+    }
+
+    @Override
+    public void print() {
+
+    }
 }
--- app/src/main/java/com/example/c0771910_w2020_mad3125_fp/classes/Customer.java
+++ app/src/main/java/com/example/c0771910_w2020_mad3125_fp/classes/Customer.java
@@ -1,10 +1,24 @@
 package com.example.c0771910_w2020_mad3125_fp.classes;
 
-public class Customer {
+import java.util.Dictionary;
+
+public class Customer implements Idisplay{
 
     private String customerId;
     private String firstName;
     private String lastName;
     private String emailId;
+    private Dictionary<String,Bill> customerBills;
 
+    public Customer(String customerId, String firstName, String lastName, String emailId) {
+        this.customerId = customerId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailId = emailId;
+    }
+
+    @Override
+    public void print() {
+
+    }
 }
