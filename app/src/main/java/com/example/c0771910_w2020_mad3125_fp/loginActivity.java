de8192002390ae85810e3f44388a8b902da6960a
ADITHYA SAI <60120972+neeliadithyasai@users.noreply.github.com>
2020-04-13 12:47:29 -0400

    [AN] Initialized view in login activity class.

--- app/src/main/java/com/example/c0771910_w2020_mad3125_fp/loginActivity.java
+++ app/src/main/java/com/example/c0771910_w2020_mad3125_fp/loginActivity.java
@@ -3,13 +3,28 @@ package com.example.c0771910_w2020_mad3125_fp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class loginActivity extends AppCompatActivity {
+
+    private TextView username;
+    private TextView password;
+    private Switch rememberme;
+    private Button login;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = findViewById(R.id.userName);
+        password = findViewById(R.id.passWord);
+        rememberme = findViewById(R.id.switchRememberMe);
+        login = findViewById(R.id.btnLogin);
+
+
     }
 }
