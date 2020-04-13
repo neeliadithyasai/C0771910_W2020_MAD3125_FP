f43fe52642e758806de6fc4b35be66350323e344
ADITHYA SAI <60120972+neeliadithyasai@users.noreply.github.com>
2020-04-13 13:57:14 -0400

    [AN] Added function for remember me and shared preferences.

--- app/src/main/java/com/example/c0771910_w2020_mad3125_fp/loginActivity.java
+++ app/src/main/java/com/example/c0771910_w2020_mad3125_fp/loginActivity.java
@@ -1,11 +1,18 @@
 package com.example.c0771910_w2020_mad3125_fp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class loginActivity extends AppCompatActivity {
 
@@ -13,6 +20,9 @@ public class loginActivity extends AppCompatActivity {
     private TextView password;
     private Switch rememberme;
     private Button login;
+    private SharedPreferences loginPreferences;
+    private SharedPreferences.Editor loginPrefsEditor;
+    private Boolean saveLogin;
 
 
     @Override
@@ -20,11 +30,94 @@ public class loginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
+        iniit();
+
+
+        login.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                login();
+            }
+        });
+
+
+
+
+
+    }
+
+    private void iniit(){
+
         username = findViewById(R.id.userName);
         password = findViewById(R.id.passWord);
         rememberme = findViewById(R.id.switchRememberMe);
         login = findViewById(R.id.btnLogin);
+        loginPreferences = getSharedPreferences("loginPrefs", MODE_PRIVATE);
+        loginPrefsEditor = loginPreferences.edit();
+        saveLogin = loginPreferences.getBoolean("saveLogin", false);
 
 
+        if(saveLogin == true)
+        {
+            username.setText(loginPreferences.getString("username", ""));
+            password.setText(loginPreferences.getString("password", ""));
+            rememberme.setChecked(true);
+            Toast.makeText(loginActivity.this,"ON",Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            Toast.makeText(loginActivity.this,"OFF",Toast.LENGTH_LONG).show();
+        }
+
+
+
+
+    }
+
+
+    private void login() {
+        String userName = username.getText().toString();
+        final String passWord = password.getText().toString();
+
+        if (userName.isEmpty() || userName.trim().length() == 0) {
+            username.setError("Enter Email ID");
+            return;
+        }
+        if(userName.equals("admin") && passWord.equals("admin@123")) {
+
+            if (rememberme.isChecked()) {
+                loginPrefsEditor.putBoolean("saveLogin", true);
+                loginPrefsEditor.putString("username", userName);
+                loginPrefsEditor.putString("password", passWord);
+                loginPrefsEditor.commit();
+
+            } else {
+                loginPrefsEditor.clear();
+                loginPrefsEditor.commit();
+            }
+
+            Intent mIntent = new Intent(loginActivity.this, homeActivity.class);
+            startActivity(mIntent);
+
+
+        }else{
+            new AlertDialog.Builder(loginActivity.this)
+                    .setTitle("Inalid Credentials!")
+                    .setMessage("enter valid username and password")
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                           username.clearComposingText();
+                           password.clearComposingText();
+                        }
+                    })
+
+                    // A null listener allows the button to dismiss the dialog and take no further action.
+                    .setNegativeButton(android.R.string.no, null)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+        }
     }
 }
