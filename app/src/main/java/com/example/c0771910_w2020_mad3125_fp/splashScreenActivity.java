b431f7bb83aed3cb7bc45c86cef71983a8bb249f
ADITHYA SAI <60120972+neeliadithyasai@users.noreply.github.com>
2020-04-12 21:53:44 -0400

    [AN] Added Code in splash screen activity.

--- app/src/main/java/com/example/c0771910_w2020_mad3125_fp/splashScreenActivity.java
+++ app/src/main/java/com/example/c0771910_w2020_mad3125_fp/splashScreenActivity.java
@@ -2,13 +2,36 @@ package com.example.c0771910_w2020_mad3125_fp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class splashScreenActivity extends AppCompatActivity {
 
+
+    private static int SPLASH_SCREEN_TIME_OUT=3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(splashScreenActivity.this,
+                        loginActivity.class);
+
+                startActivity(i);
+
+
+                finish();
+
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
     }
 }
