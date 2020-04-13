3ad62d6346b5419c26d699abb5b2d6e519e01726
ADITHYA SAI <60120972+neeliadithyasai@users.noreply.github.com>
2020-04-13 15:46:22 -0400

    [AN] initialized variables in Internet extending bill and implemented constructor.

--- app/src/main/java/com/example/c0771910_w2020_mad3125_fp/classes/Internet.java
+++ app/src/main/java/com/example/c0771910_w2020_mad3125_fp/classes/Internet.java
@@ -1,4 +1,30 @@
 package com.example.c0771910_w2020_mad3125_fp.classes;
 
-public class Internet {
+public class Internet extends Bill {
+
+    private String providerName;
+    private Double internetUsed;
+
+
+    public String getProviderName() {
+        return providerName;
+    }
+
+    public void setProviderName(String providerName) {
+        this.providerName = providerName;
+    }
+
+    public Double getInternetUsed() {
+        return internetUsed;
+    }
+
+    public void setInternetUsed(Double internetUsed) {
+        this.internetUsed = internetUsed;
+    }
+
+    public Internet(String billId, String billDate, String billType, Double billAmount, String providerName, Double internetUsed) {
+        super(billId, billDate, billType, billAmount);
+        this.providerName = providerName;
+        this.internetUsed = internetUsed;
+    }
 }
