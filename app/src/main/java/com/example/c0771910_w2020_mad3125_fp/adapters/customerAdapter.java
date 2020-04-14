24a44f0301dc5ecf8273918e9f248d4b16edf334
ADITHYA SAI <60120972+neeliadithyasai@users.noreply.github.com>
2020-04-13 23:36:49 -0400

    [AN] created new customersitem xml layout.

--- app/src/main/java/com/example/c0771910_w2020_mad3125_fp/adapters/customerAdapter.java
+++ app/src/main/java/com/example/c0771910_w2020_mad3125_fp/adapters/customerAdapter.java
@@ -1,4 +1,38 @@
 package com.example.c0771910_w2020_mad3125_fp.adapters;
 
-public class customerAdapter {
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.c0771910_w2020_mad3125_fp.classes.Customer;
+
+import java.util.ArrayList;
+
+public class customerAdapter extends RecyclerView.Adapter<customerAdapter.customerViewholder> {
+
+    ArrayList<Customer> customersArrayList;
+
+    public customerAdapter(ArrayList<Customer> customersArrayList) {
+        this.customersArrayList = customersArrayList;
+    }
+
+    @NonNull
+    @Override
+    public customerViewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull customerViewholder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class customerViewholder extends RecyclerView.ViewHolder {
+    }
 }
