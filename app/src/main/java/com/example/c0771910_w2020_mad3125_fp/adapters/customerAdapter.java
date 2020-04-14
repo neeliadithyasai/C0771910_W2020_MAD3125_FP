318488b45c2a467750a822a72f7a14ec3ab1d88d
ADITHYA SAI <60120972+neeliadithyasai@users.noreply.github.com>
2020-04-14 11:21:16 -0400

    [AN] displayed customer objects in recycler view.

--- app/src/main/java/com/example/c0771910_w2020_mad3125_fp/adapters/customerAdapter.java
+++ app/src/main/java/com/example/c0771910_w2020_mad3125_fp/adapters/customerAdapter.java
@@ -1,10 +1,15 @@
 package com.example.c0771910_w2020_mad3125_fp.adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.c0771910_w2020_mad3125_fp.R;
 import com.example.c0771910_w2020_mad3125_fp.classes.Customer;
 
 import java.util.ArrayList;
@@ -12,27 +17,40 @@ import java.util.ArrayList;
 public class customerAdapter extends RecyclerView.Adapter<customerAdapter.customerViewholder> {
 
     ArrayList<Customer> customersArrayList;
+    private Context mContext;
 
-    public customerAdapter(ArrayList<Customer> customersArrayList) {
+    public customerAdapter(ArrayList<Customer> customersArrayList,Context mContext) {
         this.customersArrayList = customersArrayList;
+        this.mContext = mContext;
     }
 
     @NonNull
     @Override
     public customerViewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View mView = LayoutInflater.from(parent.getContext()).inflate(R.layout.customersitem, parent, false);
+         customerViewholder mcustomerViewholder = new customerViewholder(mView);
+        return mcustomerViewholder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull customerViewholder holder, int position) {
+    public void onBindViewHolder(@NonNull final customerAdapter.customerViewholder holder, int position)
+    {
+        Customer mcustomer = this.customersArrayList.get(position);
+        holder.txtCustomername.setText(mcustomer.getFirstName());
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return this.customersArrayList.size();
     }
 
     public class customerViewholder extends RecyclerView.ViewHolder {
+
+        TextView txtCustomername;
+        public customerViewholder(@NonNull View mView) {
+            super(mView);
+            txtCustomername = mView.findViewById(R.id.txtCustomerDisplay);
+        }
     }
 }
--- app/src/main/java/com/example/c0771910_w2020_mad3125_fp/ui/homeActivity.java
+++ app/src/main/java/com/example/c0771910_w2020_mad3125_fp/ui/homeActivity.java
@@ -1,24 +1,45 @@
 package com.example.c0771910_w2020_mad3125_fp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.TextView;
 
 import com.example.c0771910_w2020_mad3125_fp.R;
+import com.example.c0771910_w2020_mad3125_fp.adapters.customerAdapter;
 import com.example.c0771910_w2020_mad3125_fp.classes.Customer;
 import com.example.c0771910_w2020_mad3125_fp.classes.UserPrefs;
 
+import java.util.ArrayList;
+
 public class homeActivity extends AppCompatActivity {
 
-    private TextView txtd;
+
+    private RecyclerView rVCustomersList;
+    private ArrayList<Customer> customersLIST = new ArrayList<>();
+    private com.example.c0771910_w2020_mad3125_fp.adapters.customerAdapter customerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        txtd =findViewById(R.id.txtdisplay);
+        rVCustomersList = findViewById(R.id.rvCustomerList);
+
+        populateCustomers();
+
+
+        customerAdapter = new customerAdapter(customersLIST,this);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
+        rVCustomersList.setLayoutManager(mLayoutManager);
+        rVCustomersList.setAdapter(customerAdapter);
+
+
+    }
+
+    private void populateCustomers() {
 
         // get a SharedPreferences instance
         UserPrefs prefs = new UserPrefs( this.getApplicationContext() );
@@ -26,22 +47,31 @@ public class homeActivity extends AppCompatActivity {
         // get id from server or local storage
         // then find User with that id
         String Id ="";
-        Customer customer = prefs.getUser(Id);
-       // Customer customer1= prefs.getUser(Id);
+        Customer customer1 = prefs.getUser(Id);
+         Customer customer2= prefs.getUser(Id);
 
         // operations on User, e.g.
-        customer.setCustomerId("c001");
-        customer.setFirstName("Adithyasai");
-        customer.setLastName("Neeli");
-        customer.setEmailId("neeliadithyasai@gmail.com");
+        customer1.setCustomerId("c001");
+        customer1.setFirstName("Adithyasai");
+        customer1.setLastName("Neeli");
+        customer1.setEmailId("neeliadithyasai@gmail.com");
+        customer2.setCustomerId("c002");
+        customer2.setFirstName("nikhil");
+        customer2.setLastName("Kulkarni");
+        customer2.setEmailId("kulkarninikhil@gmail.com");
 
 
         // save
-        prefs.setUser(customer);
+        prefs.setUser(customer1);
         // ...or delete
         //prefs.deleteUser(customer);
 
 
-        txtd.setText(customer.getFirstName());
+
+        customersLIST.add(new Customer("c001","trial","trial2","trial@gmail.com"));
+        customersLIST.add(new Customer("c002","Adithya","sai","as@gmail.com"));
+        customersLIST.add(customer1);
+        customersLIST.add(customer2);
+
     }
 }
