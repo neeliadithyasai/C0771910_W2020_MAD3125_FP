42f867e8987c93b8676dd454b9deaa5433a46b09
ADITHYA SAI <60120972+neeliadithyasai@users.noreply.github.com>
2020-04-15 16:40:54 -0400

    [AN] implemented recycler view in bills adapter.

--- app/src/main/java/com/example/c0771910_w2020_mad3125_fp/adapters/billsAdapter.java
+++ app/src/main/java/com/example/c0771910_w2020_mad3125_fp/adapters/billsAdapter.java
@@ -1,4 +1,42 @@
 package com.example.c0771910_w2020_mad3125_fp.adapters;
 
-public class billsAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.c0771910_w2020_mad3125_fp.model.Bill;
+
+import java.util.ArrayList;
+
+public class billsAdapter extends RecyclerView.Adapter<billsAdapter.billsViewHolder> {
+
+    public static ArrayList<Bill> billArrayList;
+
+    public billsAdapter(ArrayList<Bill> billArrayList)
+    {
+        this.billArrayList = billArrayList;
+    }
+    @NonNull
+    @Override
+    public billsAdapter.billsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull billsAdapter.billsViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class billsViewHolder extends RecyclerView.ViewHolder {
+        public billsViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
