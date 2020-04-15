4de39c2d235769b4a61e1b1517f534809a2bb034
ADITHYA SAI <60120972+neeliadithyasai@users.noreply.github.com>
2020-04-15 17:01:55 -0400

    [AN] Displayed bills of customer using the recycler view.

--- app/src/main/java/com/example/c0771910_w2020_mad3125_fp/adapters/billsAdapter.java
+++ app/src/main/java/com/example/c0771910_w2020_mad3125_fp/adapters/billsAdapter.java
@@ -1,11 +1,14 @@
 package com.example.c0771910_w2020_mad3125_fp.adapters;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.c0771910_w2020_mad3125_fp.R;
 import com.example.c0771910_w2020_mad3125_fp.model.Bill;
 
 import java.util.ArrayList;
@@ -21,22 +24,40 @@ public class billsAdapter extends RecyclerView.Adapter<billsAdapter.billsViewHol
     @NonNull
     @Override
     public billsAdapter.billsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View mView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.bill_cell, parent, false);
+        billsAdapter.billsViewHolder mBillsViewHolder = new billsAdapter.billsViewHolder(mView);
+        return mBillsViewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull billsAdapter.billsViewHolder holder, int position) {
 
+        Bill mBills = this.billArrayList.get(position);
+        holder.txtCellBillId.setText(mBills.getBillId());
+       // holder.txtCellBillAmount.setText();
+        holder.txtCellBillDate.setText(mBills.getBillDate().toString());
+        holder.txtCellBillType.setText(mBills.getBillType().toString());
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return this.billArrayList.size();
     }
 
     public class billsViewHolder extends RecyclerView.ViewHolder {
+
+        TextView txtCellBillId;
+        TextView txtCellBillDate;
+        TextView txtCellBillType;
+        TextView txtCellBillAmount;
         public billsViewHolder(@NonNull View itemView) {
             super(itemView);
+            txtCellBillId = itemView.findViewById(R.id.txtCellBillId);
+            txtCellBillDate = itemView.findViewById(R.id.txtCellBillDate);
+            txtCellBillType = itemView.findViewById(R.id.txtCellBillType);
+            txtCellBillAmount = itemView.findViewById(R.id.txtCellBillAmount);
         }
     }
 }
--- app/src/main/java/com/example/c0771910_w2020_mad3125_fp/ui/detailsDisplayActivity.java
+++ app/src/main/java/com/example/c0771910_w2020_mad3125_fp/ui/detailsDisplayActivity.java
@@ -1,11 +1,16 @@
 package com.example.c0771910_w2020_mad3125_fp.ui;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import com.example.c0771910_w2020_mad3125_fp.R;
+import com.example.c0771910_w2020_mad3125_fp.adapters.billsAdapter;
 import com.example.c0771910_w2020_mad3125_fp.model.Bill;
 import com.example.c0771910_w2020_mad3125_fp.model.Customer;
 
@@ -13,22 +18,34 @@ import java.util.ArrayList;
 
 public class detailsDisplayActivity extends AppCompatActivity {
 
-    private TextView displaydata;
+    private RecyclerView rvBillsList;
     private ArrayList billsArrayList;
+    private billsAdapter BillsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details_display);
+        ActionBar mActionBar = getSupportActionBar();
+        mActionBar.setTitle("YOUR BILLS");
 
-        displaydata = findViewById(R.id.dddisplay);
+        Intent mIntent = getIntent();
+        Customer customerObj = mIntent.getParcelableExtra("CustomerOBJ");
+        billsArrayList = customerObj.getBills();
 
-        Customer customer = (Customer) getIntent().getParcelableExtra("CustomerOBJ");
-        billsArrayList = customer.getBills();
+        rvBillsList = findViewById(R.id.rvBillsList);
+        BillsAdapter = new billsAdapter(this.billsArrayList);
 
-        displaydata.setText(customer.getEmailId() +"\n" + billsArrayList.size());
+        RecyclerView.LayoutManager mLinearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+        rvBillsList.setLayoutManager(mLinearLayoutManager);
+        rvBillsList.setAdapter(BillsAdapter);
 
 
 
+    }
+    public ArrayList<Bill> getBillsArrayList()
+    {
+        return this.billsArrayList;
     }
 }
