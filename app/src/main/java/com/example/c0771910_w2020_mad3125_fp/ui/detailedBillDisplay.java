69a3172799f0c06a4eba112cdd9d4b76da9a50dc
ADITHYA SAI <60120972+neeliadithyasai@users.noreply.github.com>
2020-04-17 18:07:07 -0400

    [AN] initialized views in detailed bill display screen.

--- app/src/main/java/com/example/c0771910_w2020_mad3125_fp/ui/detailedBillDisplay.java
+++ app/src/main/java/com/example/c0771910_w2020_mad3125_fp/ui/detailedBillDisplay.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.c0771910_w2020_mad3125_fp.R;
 import com.example.c0771910_w2020_mad3125_fp.model.Bill;
@@ -11,10 +13,16 @@ import com.example.c0771910_w2020_mad3125_fp.model.Customer;
 
 public class detailedBillDisplay extends AppCompatActivity {
 
+    private TextView billtext;
+    private ImageView billimg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailed_bill_display);
+
+        billtext = findViewById(R.id.txtbillDisplay);
+        billimg = findViewById(R.id.imgBilldisplay);
 
         Intent mIntent = getIntent();
         Bill billObj = mIntent.getParcelableExtra("billOBJ");
