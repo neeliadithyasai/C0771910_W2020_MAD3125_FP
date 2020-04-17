8671c539d6b3e25fc620e842eb4348fdf2fc65a5
ADITHYA SAI <60120972+neeliadithyasai@users.noreply.github.com>
2020-04-17 17:55:33 -0400

    [AN] Added empty activity for detailed bill display.

---
+++ app/src/main/java/com/example/c0771910_w2020_mad3125_fp/detailedBillDisplay.java
@@ -0,0 +1,14 @@
+package com.example.c0771910_w2020_mad3125_fp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class detailedBillDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed_bill_display);
+    }
+}
